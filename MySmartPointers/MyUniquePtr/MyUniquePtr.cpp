b9330da70be48322c3c1a9f9018a33641fec9cc2
Данило <@LAPTOP-7M1VSVI1>
2025-11-13 17:53:43 +0200

    structure of solution was changed.

---
+++ MySmartPointers/MyUniquePtr/MyUniquePtr.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <utility>
+#include <new>
+#include <memory>
+#include <algorithm>
+#include <stdexcept>
+
+namespace my_std {
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
